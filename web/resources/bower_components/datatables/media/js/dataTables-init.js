109c0d3baa8e7ab58a767e71a49f7d39cf9e7660
markfs <marksaren@gmail.com>
2015-06-02 23:11:54 +0800

    Removed errors, changed some css

---
+++ web/resources/bower_components/datatables/media/js/dataTables-init.js
@@ -0,0 +1,17 @@
+$(document).ready( function () {
+    $('#upcomingExams').dataTable( {
+        paging: true
+    } );
+
+    $('#pastExams').dataTable( {
+        paging: true
+    } );
+
+    $('#courseUpcomingExams').dataTable( {
+        paging: true
+    } );
+
+    $('#coursePastExams').dataTable( {
+        paging: true
+    } );
+} );
